ce058e606edf4346d2cb12a78c92224b46145378
kmryc <kmrychlik05@gmail.com>
2022-01-10 17:02:07 +0100

    Seeding, UoW, Repo, Extending API

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } 
+        public DateTime LastActive { get; set; }
+        public string Interests { get; set; }
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
